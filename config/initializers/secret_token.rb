3833743ce8d7df580b258c91fdb5f4db13ff5c04
tapirus indicus <jhuangtw.dev@gmail.com>
2011-07-01 00:40:33 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundBoard::Application.config.secret_token = 'e469c9b3df13cccbc65efe275fb3847c2579688930e29b1eb1b756ad27b8cc620310aba27a7e76e29d0341ebe1a4a64f69eee29b24f2ea60d9b1e1b4a61de0a9'
